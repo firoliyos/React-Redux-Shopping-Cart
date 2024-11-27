656c752d64806a2ec85296fc887a6646d1ee296d
firoliyos <firatesfaye44@gmail.com>
2024-11-27 04:46:00 +0300

    app

--- react-redux-shopping/src/App.js
+++ react-redux-shopping/src/App.js
@@ -6,8 +6,8 @@ import Header from './components/header';
 
 function App() {
   return (
-    <div className="App">
-      {/* <Header/> */}
+    <div>
+      <Header/>
       <Routes>
         <Route exact path='/'
         element={<Home />} 
